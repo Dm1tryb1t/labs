f738c060b17091ed23da9f21c32a62faf9244ff6
Dm1tryb1t <afanasevdima465@gmail.com>
2024-03-30 21:29:20 +0300

    succes

--- source/lab8/lab8.cpp
+++ source/lab8/lab8.cpp
@@ -3,34 +3,31 @@
 
 #include <iostream>
 #include <list>
-// #include <memory>
+#include <memory>
 // g++ source/lab8/lab8.cpp source/lab8/Hardware_and_software_protection.cpp -o build/lab8
 
 // template<class T> std::list<MyUnique_ptr<T>> lst;
 int main() {
-  
+
   // MyShared_ptr<HSP> my_ptr1(new HSP("aaa", 1));
   // // // std::cout << my_ptr1->get_protection_class_number() << std::endl;
   // std::cout << my_ptr1;
-  // auto ptr_container1 = create_unique<HSP>(HSP(), HSP("aaa", 1),
-  //                             HSP("bbb", 2), HSP("ccc", 3), HSP("ccc", 4));
-  // auto ptr_container2 = create_shared<HSP>(HSP(), HSP("aaa", 1),
-  //                             HSP("bbb", 2), HSP("ccc", 3), HSP("ccc", 4));
-  // HSP* hsp = new HSP("aaa", 1);
-  // MyShared_ptr<HSP> my_ptr2(hsp);
-  // MyShared_ptr<HSP> my_ptr1(my_ptr2);
-  // MyShared_ptr<HSP> my_ptr1(new HSP());
-  // for (const auto& el : ptr_container1) {
-  //   std::cout << el;
-  // }
-  // std::cout << std::endl;
-  // for (const auto& el : ptr_container2) {
-  //   std::cout << el;
-  // }
-  // int a = 5;
-  // int& b = a;
-  // a++;
-  // std::cout << a << ' ' << b << std::endl;
+  auto ptr_container1 = create_unique<HSP>(HSP(), HSP("aaa", 1),
+                                HSP("bbb", 2), HSP("ccc", 3), HSP("ccc", 4));
+  auto ptr_container2 = create_shared<HSP>(HSP(), HSP("aaa", 1),
+                                HSP("bbb", 2), HSP("ccc", 3), HSP("ccc", 4));
+  for (const auto& el : ptr_container1) {
+    std::cout << el;
+  }
+  std::cout << std::endl;
+  for (const auto& el : ptr_container2) {
+    std::cout << el;
+  }
+  // HSP* hsp1 = new HSP();
+  // HSP* hsp2 = new HSP();
+  // MyShared_ptr<HSP> shared1(hsp1);
+  // MyShared_ptr<HSP> shared2(shared1);
+  // MyShared_ptr<HSP> shared3(MyShared_ptr<HSP>(hsp2));
   // MyUnique_ptr<int> my_ptr2(new int(4));
   // std::cout << *my_ptr2 << std::endl;
   // Make_MyUnique(HSP("sss", 2), Point(1, 2), HSP("ddd", 3));
--- source/lab8/smartPtr.h
+++ source/lab8/smartPtr.h
@@ -10,177 +10,208 @@
 #define SMARTPTR_H
 
 class Point {
-  int x, y;
-  
+    int x, y;
+
 public:
-  Point(int x, int y) : x(x), y(y) {}
-  void print_info(std::ostream& out) const { 
-    out << "(" << x << ", " << y << ")\n";
-  }
+    Point(int x, int y) : x(x), y(y) {}
+    void print_info(std::ostream& out) const {
+        out << "(" << x << ", " << y << ")\n";
+    }
 };
 
 template<class T>
 class MyUnique_ptr {
-  T* ptr = nullptr;
+    T* ptr = nullptr;
 public:
-  MyUnique_ptr(T* ptr) : ptr(ptr) {  }
-  MyUnique_ptr(const MyUnique_ptr<T>& other) = delete;
-  MyUnique_ptr(MyUnique_ptr<T>&& other) {
-    std::swap(ptr, other.ptr);
-  }
-  ~MyUnique_ptr() {
-    // std::cout << "~MyUnique_ptr\n";
-    if (ptr) delete ptr;
-    ptr = nullptr;
-    // std::cout << "~MyUnique_ptr\n";
-  }
+    MyUnique_ptr(T* ptr) : ptr(ptr) {  }
+    MyUnique_ptr(const MyUnique_ptr<T>& other) = delete;
+    MyUnique_ptr(MyUnique_ptr<T>&& other) {
+        std::swap(ptr, other.ptr);
+    }
+    ~MyUnique_ptr() {
+        // std::cout << "~MyUnique_ptr\n";
+        if (ptr) delete ptr;
+        ptr = nullptr;
+        // std::cout << "~MyUnique_ptr\n";
+    }
 
-  T* get() const {
-    return ptr;
-  }
-  T& operator*() {
-    /* if (ptr) */ return *ptr;
-  }
-  T& operator*() const {
-    /* if (ptr) */ return *ptr;
-  }
-  T* operator->() {
-    return ptr;
-  }
-  T* operator->() const {
-    return ptr;
-  }
-  
-  MyUnique_ptr<T> operator = (const MyUnique_ptr<T>&) = delete;
-  MyUnique_ptr<T>& operator = (MyUnique_ptr<T>&& other) {
-    if (this == other) return *this;
-    if (ptr) delete ptr;
-    ptr = nullptr;
-    std::swap(ptr, other.ptr);
-    return *this;
-  }
+    T* get() const {
+        return ptr;
+    }
+    T& operator*() {
+        /* if (ptr) */ return *ptr;
+    }
+    T& operator*() const {
+        /* if (ptr) */ return *ptr;
+    }
+    T* operator->() {
+        return ptr;
+    }
+    T* operator->() const {
+        return ptr;
+    }
+
+    MyUnique_ptr<T> operator = (const MyUnique_ptr<T>&) = delete;
+    MyUnique_ptr<T>& operator = (MyUnique_ptr<T>&& other) {
+        if (this == other) return *this;
+        if (ptr) delete ptr;
+        ptr = nullptr;
+        std::swap(ptr, other.ptr);
+        return *this;
+    }
 };
 
 template<class T>
 std::ostream& operator << (std::ostream& out, const MyUnique_ptr<T>& ptr) {
-  return out << *ptr;
+    return out << *ptr;
 }
 
 template <class T>
 void create_uniqueqq(std::vector<MyUnique_ptr<T>>& vec, const T& t) {
-  vec.push_back(MyUnique_ptr<T>(new T(t)));
+    vec.push_back(MyUnique_ptr<T>(new T(t)));
 }
 
 template <class T>
 MyUnique_ptr<T> create_unique(const T& t) {
-	return MyUnique_ptr<T>(new T(t));
+    return MyUnique_ptr<T>(new T(t));
 }
 
 template <class T, class ...Args>
 void create_uniqueqq(std::vector<MyUnique_ptr<T>>& vec, const T& t, const Args& ... args) {
-  vec.push_back(MyUnique_ptr<T>(new T(t)));
-  create_uniqueqq(vec, args ...);
+    vec.push_back(MyUnique_ptr<T>(new T(t)));
+    create_uniqueqq(vec, args ...);
 }
 
 template <class T, class ...Args>
 std::vector<MyUnique_ptr<T>> create_unique(const T& t, const Args& ... args) {
-	std::vector<MyUnique_ptr<T>> vec;
-  create_uniqueqq(vec, t, args ...);
-  return vec;
+    std::vector<MyUnique_ptr<T>> vec;
+    create_uniqueqq(vec, t, args ...);
+    return vec;
 }
 
 template<class T>
 class MyShared_ptr {
-  T* ptr = nullptr;
-  int ptr_count = 0;
+    T* ptr = nullptr;
+    int* ptr_count = nullptr;
 public:
-  MyShared_ptr(T* ptr) : ptr(ptr) { ptr_count = 1; }
-  MyShared_ptr(MyShared_ptr& other) {
-    ptr = other.ptr;
-    ptr_count = ++other.ptr_count;
-  }
-  MyShared_ptr(MyShared_ptr&& other) {
-    std::swap(ptr, other.ptr);
-    std::swap(ptr_count, other.ptr_count);
-  }
-  ~MyShared_ptr() {
-    // std::cout << "~MyUnique_ptr\n";
-    if (ptr_count == 1 && ptr) {
-      delete ptr;
-      ptr = nullptr;
-      std::cout << "delete\n";
+    MyShared_ptr(T* ptr) {
+        if (ptr == nullptr) {
+            this->ptr = nullptr;
+            ptr_count = nullptr;
+        }
+        else {
+            this->ptr = ptr;
+            ptr_count = new int(1);
+        }
+        // std::cout << "create shared constructor\n";
     }
-    --ptr_count;
-    std::cout << ptr_count << '\n';
-  }
-
-
-  T* get() const {
-    return ptr;
-  }
-  T& operator*() {
-    /* if (ptr) */ return *ptr;
-  }
-  T& operator*() const {
-    /* if (ptr) */ return *ptr;
-  }
-  T* operator->() {
-    return ptr;
-  }
-  T* operator->() const {
-    return ptr;
-  }
-
-  MyShared_ptr<T> operator = (MyShared_ptr<T>& other) {
-    if (this.ptr == other.ptr) return *this;
-    if (ptr_count == 1 && ptr) delete ptr;
-    ptr = nullptr;
-    ptr = other.ptr;
-    ptr_count = ++other.ptr_count;
-  }
-  MyShared_ptr<T>& operator = (MyShared_ptr<T>&& other) {
-    if (this.ptr == other.ptr) {
-      other.ptr = nullptr;
-      other.ptr_count = 0;
-      return *this;
+    MyShared_ptr(MyShared_ptr& other) {
+        ptr = other.ptr;
+        ptr_count = other.ptr_count;
+        ++(*ptr_count);
+        // std::cout << "copy shared constructor\n";
     }
-    if (ptr_count == 1 && ptr) delete ptr;
-    ptr = nullptr;
-    --ptr_count;
-    ptr = other.ptr;
-    other.ptr = nullptr;
-    ptr_count = other.ptr_count;
-    other.ptr_count = 0;
-    return *this;
-  }
+    MyShared_ptr(MyShared_ptr&& other) {
+        std::swap(ptr, other.ptr);
+        std::swap(ptr_count, other.ptr_count);
+        // std::cout << "move shared constructor\n";
+    }
+    ~MyShared_ptr() {
+        // std::cout << "~MyUnique_ptr\n";
+        if (ptr_count && ptr) {
+            if (*ptr_count == 1) {
+                delete ptr;
+                delete ptr_count;
+                ptr = nullptr;
+                ptr_count = nullptr;
+                // std::cout << "delete shared\n";
+            }
+            else {
+                --(*ptr_count);
+                // std::cout << "increment count " << *ptr_count << '\n';
+            }
+        }
+    }
+
+    T* get() const {
+        return ptr;
+    }
+    T& operator*() {
+        /* if (ptr) */ return *ptr;
+    }
+    T& operator*() const {
+        /* if (ptr) */ return *ptr;
+    }
+    T* operator->() {
+        return ptr;
+    }
+    T* operator->() const {
+        return ptr;
+    }
+
+    MyShared_ptr<T> operator = (MyShared_ptr<T>& other) {
+        if (this.ptr == other.ptr) return *this;
+        if (ptr_count && ptr) {
+            if (*ptr_count == 1) {
+                delete ptr;
+                delete ptr_count;
+            }
+            else {
+                --(*ptr_count);
+            }
+        }
+        ptr = other.ptr;
+        ptr_count = other.ptr_count;
+        ++(*ptr_count);
+        // std::cout << "copy shared\n";
+        return *this;
+    }
+    MyShared_ptr<T>& operator = (MyShared_ptr<T>&& other) {
+        if (this.ptr == other.ptr) return *this;
+        if (ptr_count && ptr) {
+            if (*ptr_count == 1) {
+                delete ptr;
+                delete ptr_count;
+            }
+            else {
+                --(*ptr_count);
+            }
+        }
+        ptr = nullptr;
+        ptr_count = nullptr;
+        std::swap(ptr, other.ptr);
+        std::swap(ptr_count, other.ptr_count);
+        // std::cout << "move shared\n";
+        return *this;
+    }
 };
 
 template<class T>
 std::ostream& operator << (std::ostream& out, const MyShared_ptr<T>& ptr) {
-  return out << *ptr;
+    return out << *ptr;
 }
 
 template <class T>
 void create_sharedqq(std::vector<MyShared_ptr<T>>& vec, const T& t) {
-  vec.push_back(MyShared_ptr<T>(new T(t)));
+    vec.push_back(MyShared_ptr<T>(new T(t)));
 }
 
 template <class T>
 MyShared_ptr<T> create_shared(const T& t) {
-	return MyShared_ptr<T>(new T(t));
+    return MyShared_ptr<T>(new T(t));
 }
 
 template <class T, class ...Args>
 void create_sharedqq(std::vector<MyShared_ptr<T>>& vec, const T& t, const Args& ... args) {
-  vec.push_back(MyShared_ptr<T>(new T(t)));
-  create_sharedqq(vec, args ...);
+    vec.push_back(MyShared_ptr<T>(new T(t)));
+    create_sharedqq(vec, args ...);
 }
 
 template <class T, class ...Args>
 std::vector<MyShared_ptr<T>> create_shared(const T& t, const Args& ... args) {
-	std::vector<MyShared_ptr<T>> vec;
-  create_sharedqq(vec, t, args ...);
-  return vec;
+    std::vector<MyShared_ptr<T>> vec;
+    create_sharedqq(vec, t, args ...);
+    return vec;
 }
 
 
