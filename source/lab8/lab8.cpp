f4e8f9599a4d7b8141a1f2f5bfdcee221cee2ec5
Dm1tryb1t <afanasevdima465@gmail.com>
2024-04-06 08:56:55 +0300

    done

--- source/lab8/lab8.cpp
+++ source/lab8/lab8.cpp
@@ -9,7 +9,7 @@
 // template<class T> std::list<MyUnique_ptr<T>> lst;
 int main() {
 
-  // MyShared_ptr<HSP> my_ptr1(new HSP("aaa", 1));
+  {// MyShared_ptr<HSP> my_ptr1(new HSP("aaa", 1));
   // // // std::cout << my_ptr1->get_protection_class_number() << std::endl;
   // std::cout << my_ptr1;
   // auto ptr_container1 = create_unique<HSP>(HSP(), HSP("aaa", 1),
@@ -28,10 +28,21 @@ int main() {
   MyShared_ptr<HSP> shared1(hsp1);
   MyShared_ptr<HSP> shared2(shared1);
   MyShared_ptr<HSP> shared3 = MyShared_ptr<HSP>(MyShared_ptr<HSP>(hsp2));
-  std::cout << shared1.get_ptrCount() << std::endl;
+  std::cout << "TEST " << shared1.get_ptrCount() << std::endl;
   std::cout << shared2.get_ptrCount() << std::endl;
   std::cout << shared3.get_ptrCount() << std::endl;
-  // MyUnique_ptr<int> my_ptr2(new int(4));
+  {
+      MyShared_ptr<HSP> shared4(shared1);
+       std::cout << shared1.get_ptrCount() << std::endl;
+  std::cout << shared2.get_ptrCount() << std::endl;
+  std::cout << shared3.get_ptrCount() << std::endl;
+    std::cout << shared4.get_ptrCount() << std::endl;
+  }
+         std::cout << shared1.get_ptrCount() << std::endl;
+  std::cout << shared2.get_ptrCount() << std::endl;
+  std::cout << shared3.get_ptrCount() << std::endl;
+  }
+  std::cout<<"END"<<std::endl;// MyUnique_ptr<int> my_ptr2(new int(4));
   // std::cout << *my_ptr2 << std::endl;
   // Make_MyUnique(HSP("sss", 2), Point(1, 2), HSP("ddd", 3));
   return 0;
